43728c5788214e94526eb48848de3636d842be78
Du-Wayne Frieslaar <Du-Wayne.Frieslaar@capaciti.org.za>
2024-11-20 14:31:38 +0200

    message

--- index.js
+++ index.js
@@ -22,3 +22,26 @@ gitButton.addEventListener('click', function(){
     alert("Of to GitHub!"); // Display an alert message
     gitButton.style.backgroundColor = "red"; // Change the background color of the button to red
 });
+
+
+// Function to open the modal
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+// Close the modal if the user clicks outside of the modal content
+window.onclick = function(event) {
+    var modals = document.querySelectorAll('.modal');
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
