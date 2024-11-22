2e1028edc05566d15ee66a663d598f44b849f87d
Du-Wayne Frieslaar <Du-Wayne.Frieslaar@capaciti.org.za>
2024-11-22 11:25:39 +0200

    added top projects

--- index.js
+++ index.js
@@ -46,3 +46,29 @@ window.onclick = function(event) {
         }
     });
 }
+
+// Get the modal
+s
+
+
+    const openModalButton = document.getElementById("openModal");
+    const modal = document.getElementById("myModal");
+    const closeModalButton = document.getElementById("closeModal");
+
+    // Open modal
+    openModalButton.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+
+    // Close modal
+    closeModalButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    // Close modal when clicking outside of the modal content
+    window.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+
